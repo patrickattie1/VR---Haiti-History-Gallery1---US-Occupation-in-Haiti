4fd126ec9da41e8b2a20561901e18d73084822f4
Patrick ATTIE <patrick.attie@esih.edu>
2020-02-09 16:24:22 -0500

    Commit #8 - February 9th, 2020 - Including 3 scenes (Desktop, WebGL, Quest)

---
+++ Assets/Datafiles/Scripts/PlayVideos.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+
+public class PlayVideos : MonoBehaviour
+{
+    public VideoPlayer vp1, vp2, vp3;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        vp1.url = System.IO.Path.Combine(Application.streamingAssetsPath, "DignitaryOfficersSaluteHaitianGendarmerie.mp4");
+        vp2.url = System.IO.Path.Combine(Application.streamingAssetsPath, "CrowdNnativesAttMarketplace.mp4");
+        vp3.url = System.IO.Path.Combine(Application.streamingAssetsPath, "HaitianPrisonersToMarinePrison.mp4");
+
+        vp1.Play();
+        vp2.Play();
+        vp3.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
